60b5f975f4ab2ca62d895b51feebefe7c3e0c642
Alekseev.Nikolai@phystech.edu <Alekseev.Nikolai@phystech.edu>
2019-12-06 00:46:16 +0300

    vector 3

--- MyVector/MyVector.h
+++ MyVector/MyVector.h
@@ -1,41 +1,181 @@
 #pragma once
 #include<algorithm>
+#include <memory>
+#include <iterator>
 
-class myvector
+template <typename T, typename alloc>
+struct vectorBase
+{
+	alloc allo;
+	T* elem;
+	size_t sz;
+	size_t space;
+
+	vectorBase(alloc a, size_t n)
+		: allo{ a }, elem{ a.allocate(n) }, sz{ n }, space{ n }{}
+
+	vectorBase(vectorBase&& arg);
+
+	vectorBase& operator=(vectorBase&& arg);
+
+	~vectorBase() { allo.deallocate(elem, space); }
+};
+
+template <typename T, typename alloc>
+vectorBase<T, alloc>::vectorBase(vectorBase&& arg)
+{
+	arg.sz = 0;
+	arg.elem = nullptr;
+	arg.space = 0;
+}
+
+template <typename T, typename alloc>
+vectorBase<T, alloc>& vectorBase<T, alloc>::operator=(vectorBase&& arg)
+{
+	swap(this->elem, arg.elem);
+	swap(this->sz, arg.sz);
+	swap(this->space, arg.space);
+	return *this;
+}
+
+template<typename T, typename alloc = std::allocator<T>>
+class myvector : private vectorBase<T, alloc>
 {
 public:
-	explicit myvector(unsigned long long s)
-		:sz{ s },
-		elem{ new double[s] }
-	{
-		for (unsigned long long i = 0; i < sz; ++i)
-				elem[i] = 0;
-	}
+	explicit myvector(size_t n, T d = T(), alloc allo = alloc());
 
-	myvector(std::initializer_list<double> lst)
-		:sz{ lst.size() },
-		elem{ new double[sz] }
-	{
-		std::copy(lst.begin(), lst.end(), elem);
-	}
+	myvector(std::initializer_list<T> lst, const alloc& allo = alloc());
 
-	myvector(const myvector& nw);
-	myvector& operator= (const myvector&);
-	double& operator[] (unsigned long long n) { return elem[n]; }
-	double operator[] (unsigned long long n) const { return elem[n]; }
-	myvector(myvector&& nw);
-	myvector& operator= (myvector&&);
+	myvector(const alloc& allo = alloc()) : vectorBase<T, alloc>(allo, 1) {}
 
-	~myvector()
-	{
-		delete[] elem;
-	}
+	myvector(const myvector& arg);
+	myvector& operator= (const myvector& v);
 
-	unsigned long long size() const { return sz; }
-	double get(unsigned long long n) const { return elem[n]; }
-	void set(unsigned long long n, double v) { elem[n] = v; }
+	myvector(myvector&& a) = default;
+	myvector& operator= (myvector && a) = default;
 
-private:
-	unsigned long long sz;
-	double* elem;
+	~myvector();
+
+	T& at(size_t n);
+	const T& at(size_t n) const;
+
+	T& operator[] (size_t n);
+	const T& operator[] (size_t n) const;
+
+	size_t size() const { return this->sz; }
+
+	size_t capacity() const { return this->space; }
+
+	void reserve(size_t newalloc);
+
+	void resize(size_t & newsize, T val = T());
+
+	void push_back(const T & d);
 };
+
+template <typename T, typename alloc>
+myvector<T, alloc>::myvector(size_t n, T d, alloc allo)
+	:vectorBase<T, alloc>(allo, n)
+{
+	for (int i = 0; i < n; ++i)
+		this->allo.construct(&this->elem[i], d);
+}
+
+template <typename T, typename alloc>
+myvector<T, alloc>::myvector(std::initializer_list<T> lst, const alloc& allo)
+	:vectorBase<T, alloc>(allo, lst.size())
+{
+	for (int i = 0; i < lst.size(); ++i)
+		this->allo.construct(&this->elem[i], *(lst.begin() + 1));
+}
+
+template <typename T, typename alloc>
+myvector<T, alloc>::~myvector()
+{
+	for (int i = 0; i < this->sz; ++i)
+		this->allo.destroy(this->elem + i);
+}
+
+template <typename T, typename alloc>
+myvector<T, alloc>::myvector(const myvector& arg)
+	:vectorBase<T, alloc>(arg.allo, arg.sz)
+{
+	for (int i = 0; i < arg.sz; ++i)
+		this->allo.construct(&this->elem[i], arg[i]);
+}
+
+template <typename T, typename alloc>
+myvector<T, alloc>& myvector<T, alloc>::operator=(const myvector<T, alloc>& arg)
+{
+	if (this == &arg)
+		return *this;
+	vectorBase<T, alloc> b{ this->allo, arg.sz };
+	uninitialized_copy(arg.begin(), arg.end(), b.elem);
+	swap(this->elem, b.elem);
+	swap(this->sz, b.sz);
+	swap(this->space, b.space);
+	for (size_t i = 0; i < b.sz; ++i)
+		b.allo.destroy(b.elem + i);//free memory used for vector before
+	return *this;
+}
+
+template <typename T, typename alloc>
+void myvector<T, alloc>::reserve(size_t newalloc)
+{
+	if (newalloc <= this->space) return;
+	vectorBase<T, alloc> b(this->allo, newalloc);
+	uninitialized_copy(b.elem, &b.elem[this->sz], this->elem);
+	for (size_t i = 0; i < this->sz; ++i)
+	{
+		this->allo.destroy(&this->elem[i]);
+	}
+	swap(this->sz, b.sz);
+	swap(this->space, b.space);
+	swap(this->allo, b.allo);
+	swap(this->elem, b.elem);
+}
+template <typename T, typename alloc>
+void myvector<T, alloc>::push_back(const T& val)
+{
+	if (!this->space)
+		reserve(8);
+	else if (this->sz == this->space)
+		reserve(2 * this->space);
+	this->allo.construct(&this->elem[this->sz], val);
+	++this->sz;
+}
+
+template <typename T, typename alloc>
+void myvector<T, alloc>::resize(size_t& newsize, T val)
+{
+	reserve(newsize);
+	for (size_t i = this->sz; i < newsize; ++i) this->allo.construct(&this->elem[i], val);
+	for (size_t i = newsize; i < this->sz; ++i) this->allo.destroy(&this->elem[i]);
+	this->sz = newsize;
+}
+
+template <typename T, typename alloc>
+T& myvector<T, alloc>::at(size_t n)
+{
+	if (n < 0 || this->sz <= n) throw sstd::out_of_range("Out of range");
+	return this->elem[n];
+}
+
+template <typename T, typename alloc>
+const T& myvector<T, alloc>::at(size_t n) const
+{
+	if (n < 0 || this->sz <= n) throw std::out_of_range("Out of range");
+	return this->elem[n];
+}
+
+template <typename T, typename alloc>
+T& myvector<T, alloc>::operator[] (size_t n)
+{
+	return this->elem[n];
+}
+
+template <typename T, typename alloc>
+const T& myvector<T, alloc>::operator[] (size_t n) const
+{
+	return this->elem[n];
+}
--- MyVector/main.cpp
+++ MyVector/main.cpp
@@ -1,18 +1,6 @@
+#include "MyVector.h"
 #include <iostream>
-#include "myvector.h"
-#include <cmath>
 
 int main()
 {
-	myvector v1{ 1,3,5,7,9, };
-	myvector v2{ 2,4,6,8,10 };
-	std::cout << v2.size() << " " << v1.size() << std::endl;
-	v2 = v1;
-	std::cout << "v1: ";
-	for (unsigned long long i = 0; i < v1.size(); ++i)
-		std::cout << v1[i] << " ";
-	std::cout << std::endl;
-	std::cout << "v2: ";
-	for (unsigned long long i = 0; i < v2.size(); ++i)
-		std::cout << v2[i] << " ";
 }
